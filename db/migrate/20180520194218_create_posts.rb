f9fc9051c4ee8fe6a05eda1a1e78363063e575d5
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-20 15:42:43 -0400

    created models

---
+++ db/migrate/20180520194218_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :description
+      t.string :post_photo_url
+      t.string :posted_user_name
+      t.refrences :city
+      t.string :timestamp
+
+      t.timestamps
+    end
+  end
+end
