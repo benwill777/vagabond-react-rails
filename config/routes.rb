a0874419e50db38c830c691532543f5484bfea10
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-20 16:54:50 -0400

    created city controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'cities/index'
+    get 'cities/show'
+    get 'cities/delete'
+    get 'cities/update'
+    get 'cities/create'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :cities do
